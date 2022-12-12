ffaebc1467524f6c331ba419726f5afa383c96db
Abdulqaweeyu <101641686+ABQ4539@users.noreply.github.com>
2022-12-12 22:25:22 +0100

    update

--- components/About.js
+++ components/About.js
@@ -4,7 +4,8 @@ export default function About() {
     return (
         <div className="about">
             <h1>About</h1>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+                I try to keep up with security and best practices, and I'm always looking for new things to learn.</p>
         </div>
     )
 } 
